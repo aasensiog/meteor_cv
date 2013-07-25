de4d090399b7cd9faf19b41d5d8bb39a13da231d
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-07-25 16:22:52 +0200

    Initial commit

---
+++ main/app/client/lib/main.js
@@ -0,0 +1,19 @@
+if (Meteor.isClient) {
+  /*
+  Template.hello.greeting = function () {
+    return "Welcome to main.";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+  */
+  $(function() {
+  	$('button').button();
+  });
+}
+
