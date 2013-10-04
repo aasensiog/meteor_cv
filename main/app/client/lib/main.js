819f67253c829ca3f103e65de3ab02548970432b
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-10-04 14:18:48 +0200

    added account management

--- main/app/client/lib/main.js
+++ main/app/client/lib/main.js
@@ -1,19 +1,13 @@
 if (Meteor.isClient) {
-  /*
-  Template.hello.greeting = function () {
-    return "Welcome to main.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-  */
-  $(function() {
-  	$('button').button();
-  });
+Accounts.ui.config({
+  requestPermissions: {
+    facebook: ['user_likes'],
+    github: ['user', 'repo']
+  },
+  requestOfflineToken: {
+    google: true
+  },
+  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+});
 }
 
