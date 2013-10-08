7e554d15c5543399fc328e000db24cd4104765c6
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-10-08 16:51:15 +0200

    some changes

--- main/app/client/lib/index.js
+++ main/app/client/lib/index.js
@@ -34,12 +34,10 @@ if (Meteor.isClient) {
 			// GeoChart selections return an array of objects with a row property; no column information
 				addAndUpdateChart(region.region);
 			});
-			var user = Meteor.users.findOne({_id:Meteor.userId()});
-			if (user) {
-				var arr = user.profile.countries;
-				if (arr && arr.length > 0) {
-				  countriesVisited = arr;
-				}
+			var user = Meteor.user(),
+                arr = user && user.profile && user.profile.countries;
+            if (arr && arr.length > 0) {
+				countriesVisited = arr;
 			} else {
 				saveOnDB(countriesVisited);
 			}
@@ -47,10 +45,11 @@ if (Meteor.isClient) {
 			chart.draw(google.visualization.arrayToDataTable(doMap(countriesVisited)), options);
 		},
 		saveOnDB = function(countriesVisited) {
-			var user = Meteor.user();
+            var that = this,
+                user = Meteor.user();
 			if (user) {
 				Meteor.users.update({
-					_id: this._userId
+					_id: user._id
 				}, 
 				{ 
 					$set: {
@@ -58,11 +57,14 @@ if (Meteor.isClient) {
 					}
 				});
 			}
-		  	//localStorage.setItem('countriesVisited', JSON.stringify(countriesVisited));
 		};
-
-		google.load('visualization', '1', {'packages': ['geochart']});
-    	google.setOnLoadCallback(drawRegionsMap);
+    
+    Template.map.userData = function() {
+        return Meteor.user().emails[0].address;
+    };
+    
+    google.load('visualization', '1', {'packages': ['geochart']});
+    google.setOnLoadCallback(drawRegionsMap);
 
 }
 
