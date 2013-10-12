95c9898ae15c26136db39926a5e559299442e61c
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-10-12 19:41:29 +0200

    map added

--- main/app/client/lib/index.js
+++ main/app/client/lib/index.js
@@ -1,144 +1,100 @@
 if (Meteor.isClient) {
-    /*
-	var map,
-		options = {
-		  keepAspectRatio: true,
-		  datalessRegionColor: 'PaleGoldenrod'
-		},
-		countriesVisited = [];
-		doMap = function(countries) {
-		  var x = [];
-		  $.map(countries, function(el,i) {
-		    return x.push([el]);
-		  });
-		  var visited = Math.max(x.length - 1, 0);
-		  var total = Meteor.availableCountries.length;
-		  //$('#percent').html('<p>'+visited+'/'+total+' '+Math.floor((visited/total)*100)+'%</p>');
-		  return x;
-		},
-		addAndUpdateChart = function(region) {
-		  if (region) {
-		    if (countriesVisited.indexOf(region) == -1) {
-		      countriesVisited.push(region);
-		    } else {
-		      countriesVisited.splice(countriesVisited.indexOf(region), 1);
-		    }
-
-		    chart.draw(google.visualization.arrayToDataTable(doMap(countriesVisited)), options);
-		    saveOnDB(countriesVisited);
-		  }
-		},
-		drawRegionsMap = function() {
-            map = new AmCharts.AmMap();
-            //map.pathToImages = "http://www.ammap.com/lib/images/";
-            map.panEventsEnabled = true;
-            map.backgroundColor = "#666666";
-            map.backgroundAlpha = 1;
-        
-            var dataProvider = {
-                mapVar: AmCharts.maps.worldHigh,
-                getAreasFromMap: true,
-                areas: []
-            };
-        
-            map.dataProvider = dataProvider;
-        
-            map.areasSettings = {
-                autoZoom: false,
-                selectable:true,
-                color: "#CDCDCD",
-                colorSolid: "#5EB7DE",
-                selectedColor: "#5EB7DE",
-                outlineColor: "#666666",
-                rollOverColor: "#88CAE7",
-                rollOverOutlineColor: "#FFFFFF"
-            };
-            map.addListener('clickMapObject', function (event) {
-              //console.debug(event);
-              var obj = event.mapObject.id;
-              updateMap(true);
-            });
-        
-            map.write("mapdiv");
-            /*
-			google.visualization.events.addListener(chart, 'regionClick', function (region) {
-			// GeoChart selections return an array of objects with a row property; no column information
-				addAndUpdateChart(region.region);
-			});
-            
-			var user = Meteor.user(),
-                arr = user && user.profile && user.profile.countries;
-            if (arr && arr.length > 0) {
-				countriesVisited = arr;
-			} else {
-				saveOnDB(countriesVisited);
-			}
-			
-		},
-		saveOnDB = function(countriesVisited) {
-            var that = this,
-                user = Meteor.user();
-			if (user) {
-				Meteor.users.update({
-					_id: user._id
-				}, 
-				{ 
-					$set: {
-						'profile.countries': countriesVisited
-					}
-				});
-			}
-		};
     
-    Template.index.userData = function() {
-        drawRegionsMap();
-        return Meteor.user().emails[0].address;
+    var countries, //array of countrycodes
+        map;
+
+    var generateColors = function(countries) {
+      var colors = {};
+      $.map(countries, function(element, i) {
+        colors[element] = '#66CDAA'; 
+      });
+      return colors;
     };
-    */
 
-    
-    
-    
-    
+    var updateMap = function() {
+      jQuery('#map').vectorMap('set', 'colors', generateColors(countries));
+    };
+
+    var addRegion = function(countryCode) {
+      var index = countries.indexOf(countryCode);
+      if (index < 0) {
+        countries.push(countryCode);  
+      } else {
+        countries.splice(index, 1);
+      }
+      
+      updateMap();
+      saveOnDB(countries);
+
+    };
+
+    var saveOnDB = function(countriesVisited) {
+      var that = this,
+        user = Meteor.user();
+      if (user) {
+        Meteor.users.update({
+          _id: user._id
+        },
+        {
+          $set: {
+          'profile.countries': countriesVisited
+          }
+        });
+      }
+    };
     //-------------------------------------------------------------------//
     //-----------------------LOGIN & REGISTER----------------------------//
     //-------------------------------------------------------------------//
     
     Template.data.userData = function() {
-        return {name: Meteor.user().profile.name};
+        return {
+          name: Meteor.user().profile.name
+        };
     };
     
     Template.data.rendered = function() {
-        $('#map').vectorMap({map: 'world_merc_en'});
-    };
-    Template.data.events(
-    {
-        'click #logout' : function(event) {
-            return Meteor.logout();
+        countries = Meteor.user().profile.countries || [];
+
+        if (!map) {
+          map = jQuery('#map').vectorMap({
+            map: 'world_en',
+            backgroundColor: '#333333',
+            color: '#ffffff',
+            hoverOpacity: 0.7,
+            selectedColor: '#666666',
+            enableZoom: true,
+            showTooltip: true,
+            scaleColors: ['#C8EEFF', '#006491'],
+            normalizeFunction: 'polynomial',
+            colors: generateColors(countries),
+            onRegionClick: function(element, code, region) {
+              addRegion(code);
+            }
+          });
         }
-    }
+
+    };
+
+    Template.data.events(
+      {
+          'click #logout' : function(event) {
+              return Meteor.logout();
+          }
+      }
     );
     
     Template.login.events({
 
     'submit #login-form' : function(e, t){
       e.preventDefault();
-      // retrieve the input field values
+
       var email = t.find('#login-email').value,
           password = t.find('#login-password').value;
 
-        // Trim and validate your fields here.... 
-
-        // If validation passes, supply the appropriate fields to the
-        // Meteor.loginWithPassword() function.
         Meteor.loginWithPassword(email, password, function(err){
         if (err)
-          // The user might not have been found, or their passwword
-          // could be incorrect. Inform the user that their
-          // login attempt has failed. 
             alert('login fail');
         else
-          // The user has been logged in.
             alert('login ok');
       });
          return false; 
@@ -148,19 +104,16 @@ if (Meteor.isClient) {
     Template.register.events({
     'submit #register-form' : function(e, t) {
       e.preventDefault();
+
       var email = t.find('#account-email').value,
           password = t.find('#account-password').value,
           name = t.find('#account-name').value;
-
-        // Trim and validate the input
 
       Accounts.createUser({email: email, password : password, profile: {name: name}}, function(err){
           if (err) {
             alert('register fail');
           } else {
-              alert('register ok');
-            // Success. Account has been created and the user
-            // has logged in successfully. 
+            alert('register ok');
           }
 
         });
@@ -168,8 +121,6 @@ if (Meteor.isClient) {
       return false;
     }
   });
-        
-        
 
 }
 
